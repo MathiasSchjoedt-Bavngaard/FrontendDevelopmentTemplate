38e25ed48a623406aec4968ef0dec6e307650b68
MathiasSchjoedt-Bavngaard <81029612+MathiasSchjoedt-Bavngaard@users.noreply.github.com>
2023-01-21 08:57:36 +0100

    added WPF Yml and dependencies

---
+++ WPF/Dept_Book.Tests/ThisAppInfoTest.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using NUnit.Framework;
+using MyWPFApp;
+
+namespace MyWPFApp.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var thisAppInfo = new ThisAppInfo();
+        }
+
+        [Test]
+        public void GetDisplayName()
+        {
+            var displayName = ThisAppInfo.GetDisplayName();
+            Assert.AreEqual("Not packaged", displayName);
+        }
+
+        [Test]
+        public void GetAppInstallerUri()
+        {
+            var appInstallerUri = ThisAppInfo.GetAppInstallerUri();
+
+            Assert.AreEqual("Not packaged", appInstallerUri);
+        }
+    }
+}
